04e71f1f25803dbab3e77e295f39ea0dde1696ab
Alex Tsao <caowenjieko@gmail.com>
2024-07-23 15:01:41 +0800

    test add zh-tw

--- utils/links.ts
+++ utils/links.ts
@@ -10,4 +10,11 @@ export const links: NavLink[] = [
   { href: "/favorites", label: "favorites" },
   { href: "/cart", label: "cart" },
   { href: "/orders", label: "orders" },
+  { href: "/admin/sales", label: "儀表板" },
+];
+
+export const adminLinks: NavLink[] = [
+  { href: "/admin/sales", label: "sales" },
+  { href: "/admin/products", label: "my products" },
+  { href: "/admin/products/create", label: "create product" },
 ];
